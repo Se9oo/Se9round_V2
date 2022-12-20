f9384a52b53f448abb6932f1f491d58a7bc0d405
se9oo <se9oo@kakao.com>
2022-12-20 12:30:47 +0900

    색상 코드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
 	content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
 	darkMode: 'class',
 	theme: {
+		colors: {
+			main: '#439A97',
+			red: '#EB455F',
+			yellow: '#FFE15D',
+		},
 		extend: {},
 	},
 	plugins: [],
