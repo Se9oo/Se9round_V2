34dbb0bed96d5184637c1982491e7eeac2b9755f
se9oo <se9oo@kakao.com>
2022-12-21 15:42:36 +0900

    markdown 파일 경로 상수 값 사용하도록 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import matter from 'gray-matter';
 import MainLayout from '~/components/layout/MainLayout';
 import PostList from '~/components/posts/PostList';
 import { PostFileType } from '~/types/post';
+import { MARKDOWN_FILE_PATH } from '~/constants/url';
 
 const Selog = (props: { posts: PostFileType[] }) => {
 	const { posts } = props;
@@ -18,12 +19,12 @@ const Selog = (props: { posts: PostFileType[] }) => {
 export default Selog;
 
 export const getStaticProps = async () => {
-	const files = fs.readdirSync('public/posts');
+	const files = fs.readdirSync(MARKDOWN_FILE_PATH);
 
 	const posts = files.map((file) => {
 		const fileName = file.replace('.md', '');
 
-		const content = fs.readFileSync(`public/posts/${file}`, 'utf-8');
+		const content = fs.readFileSync(`${MARKDOWN_FILE_PATH}/${file}`, 'utf-8');
 		const parsedContent = matter(content);
 
 		const { data } = parsedContent;
