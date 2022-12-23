d2442df41049121ad4f59433d87e5a0631f510e7
se9oo <se9oo@kakao.com>
2022-12-23 16:42:31 +0900

    MainLayout 스타일 수정

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,9 +3,9 @@ import Header from '../header/Header';
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
 	return (
-		<main className="relative w-screen max-w-[768px] mx-auto my-0 bg-white dark:bg-dark dark:text-darkText leading-6 traking-[-0.5px]">
+		<main className="relative w-full max-w-[768px] mx-auto my-0 bg-white dark:bg-dark dark:text-darkText leading-6 traking-[-0.5px]">
 			<Header />
-			<section className="mt-14 sm:mt-[72px] p-6 sm:p-0">{children}</section>
+			<section className="mt-14 sm:mt-[72px] p-6 sm:p-4">{children}</section>
 		</main>
 	);
 };
