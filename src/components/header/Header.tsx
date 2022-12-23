0f0d4ea74eb65dfb3e25ea502095bfbfe5e8cf72
se9oo <se9oo@kakao.com>
2022-12-23 16:41:34 +0900

    Header 스타일 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -22,15 +22,15 @@ const Header = () => {
 	};
 
 	return (
-		<header className="fixed top-0 left-0 w-screen h-14 sm:h-[72px] shadow-sm z-header bg-white dark:bg-dark">
-			<div className="w-full max-w-[768px] h-full mx-auto my-0 flex justify-between items-center px-6 sm:px-0 py-3 sm:py-6">
+		<header className="fixed top-0 left-0 w-full h-14 sm:h-[72px] shadow-sm z-header bg-white dark:bg-dark">
+			<div className="w-full max-w-[768px] h-full mx-auto my-0 flex justify-between items-center px-6 sm:px-2 py-3 sm:py-6">
 				<h1>
 					<button type="button" onClick={handleMoveHome}>
 						selog
 					</button>
 				</h1>
 				<ul className="flex justify-start items-center">
-					<li className="flex justify-center items-center mx-4">
+					<li className="flex justify-center items-center">
 						<button type="button" onClick={handleTheme}>
 							{theme === 'light' ? (
 								<LightModeIcon className="stroke-red" />
@@ -40,7 +40,7 @@ const Header = () => {
 						</button>
 					</li>
 					<li>
-						<button type="button" className="block sm:hidden">
+						<button type="button" className="block sm:hidden ml-2">
 							<MobileMenuIcon />
 						</button>
 					</li>
