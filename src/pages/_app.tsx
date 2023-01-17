9ba7980b82ebe6a9aae3a02ed824b5f1e2233af3
se9oo <se9oo@kakao.com>
2023-01-17 10:04:36 +0900

    head > title, description, meta tag 위치 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import '~/styles/global.css';
@@ -6,6 +7,15 @@ import '~/styles/global.css';
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<ThemeProvider attribute="class">
+			<Head>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width,
+					maximum-scale=1, minimum-scale=1, user-scalable=no"
+				/>
+				<title>se9round.dev</title>
+				<meta name="description" content="se9oo 개발 블로그" />
+			</Head>
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
