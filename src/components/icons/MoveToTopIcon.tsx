2db40051d9f2a8400f50c6dbdd71866b546cefb1
se9oo <se9oo@kakao.com>
2023-01-25 15:42:12 +0900

    MoveToTopIcon 수정

--- src/components/icons/MoveToTopIcon.tsx
+++ src/components/icons/MoveToTopIcon.tsx
@@ -4,7 +4,7 @@ const MoveToTopIcon = () => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			className="icon icon-tabler icon-tabler-chevrons-up"
+			className="icon icon-tabler icon-tabler-arrow-big-up-lines"
 			width={24}
 			height={24}
 			viewBox="0 0 24 24"
@@ -15,7 +15,7 @@ const MoveToTopIcon = () => {
 			strokeLinejoin="round"
 		>
 			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-			<path d="M7 11l5 -5l5 5m-10 6l5 -5l5 5" />
+			<path d="M9 12h-3.586a1 1 0 0 1 -.707 -1.707l6.586 -6.586a1 1 0 0 1 1.414 0l6.586 6.586a1 1 0 0 1 -.707 1.707h-3.586v3h-6v-3zm0 9h6m-6 -3h6" />
 		</svg>
 	);
 };
