611a2e67f25d63f019e24a28b4c05d1b7883aa2d
se9oo <se9oo@kakao.com>
2023-01-31 09:16:59 +0900

    _document > fonts.google preconnect 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ const Document = () => {
 	return (
 		<Html>
 			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="annonymous" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
 					rel="stylesheet"
