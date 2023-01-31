a0363fd73390c0128a0a2594462f7b9750204ee2
se9oo <se9oo@kakao.com>
2023-01-31 16:40:58 +0900

    게시글 상세 > 제목 letter-spacing 적용

--- src/components/posts/PostDetail.tsx
+++ src/components/posts/PostDetail.tsx
@@ -24,7 +24,7 @@ const PostDetail = ({ postData }: { postData: PostDataType }) => {
 						<FloatingActions post={postData} />
 					</div>
 					<div className="sm:mt-20 sm:text-start">
-						<h1 className="font-bold text-[40px] leading-[48px] mb-4 sm:mb-12">{title}</h1>
+						<h1 className="font-bold text-[40px] leading-[48px] tracking-[-0.5px] mb-4 sm:mb-12">{title}</h1>
 						{tags && tags.length > 0 ? <TagList tags={tags} customStyle="justify-start mb-4" /> : null}
 						<span className="block mb-10">{date}</span>
 						<div id="markdown-wrapper" className="text-lg">
