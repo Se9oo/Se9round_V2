31d17221e92db6ddf160f2c244d06ac5b0a7f6bc
se9oo <se9oo@kakao.com>
2023-02-03 11:39:55 +0900

    PostCard > 썸네일 이미지 > markdown에서 설정한 이미지 노출되도록 수정

--- public/posts/모바일-브라우저에서-localhost-접속하기.md
+++ public/posts/모바일-브라우저에서-localhost-접속하기.md
@@ -2,7 +2,7 @@
 title: '모바일 브라우저에서 localhost 접속하기'
 metaTitle: '모바일 브라우저에서 localhost 접속하기'
 description: 'localhost를 모바일 브라우저에서 접속하는 방법'
-socialImage: images/pic1.jpg
+socialImage: /images/dongdong.jpeg
 date: '2023-02-01'
 timestamp: 202302010000
 tags:
--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -7,7 +7,7 @@ import { convertSpaceToDash } from '~/utils/format';
 import { PostMetaDataType } from '~/types/post';
 
 const PostCard = ({ metaData }: { metaData: PostMetaDataType }) => {
-	const { title, description, date, tags } = metaData;
+	const { title, description, date, tags, socialImage } = metaData;
 
 	const handleMovePostDetailPage = () => {
 		router.push(`${PAGE_URLS.POST}/${convertSpaceToDash(title)}`);
@@ -20,16 +20,9 @@ const PostCard = ({ metaData }: { metaData: PostMetaDataType }) => {
 			onKeyDown={undefined}
 		>
 			<div className="relative w-full h-[132px] sm:h-auto sm:w-[36%] mb-4 sm:mb-0 mr-2 sm:mr-12">
-				<Image
-					src="/images/dongdong.jpeg"
-					alt="post-thumbnail"
-					fill
-					priority
-					className="rounded-md object-cover"
-					sizes="100vw"
-				/>
+				<Image src={socialImage} alt="post-thumbnail" fill priority className="rounded-md object-cover" sizes="100vw" />
 			</div>
-			<div className="relative sm:w-[65%] flex flex-col">
+			<div className="relative sm:w-[65%] h-full flex flex-col">
 				<h3 className="font-bold text-[22px] mb-4 break-all sm:break-normal tracking-[-0.5px]">{title}</h3>
 				<p className="block mb-4 sm:mb-0 break-all sm:break-normal tracking-[-0.5px]">{description}</p>
 				{tags && tags.length > 0 ? (
