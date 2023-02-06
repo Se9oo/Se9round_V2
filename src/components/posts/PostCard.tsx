cfdb51a0665a7c77029fca977c3c519d9e641a30
se9oo <se9oo@kakao.com>
2023-02-06 17:16:12 +0900

    PostCard > 게시글 제목 line-height 값 설정

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -23,7 +23,9 @@ const PostCard = ({ metaData }: { metaData: PostMetaDataType }) => {
 				<Image src={socialImage} alt="post-thumbnail" fill priority className="rounded-md object-cover" sizes="100vw" />
 			</div>
 			<div className="relative sm:w-[65%] h-full flex flex-col">
-				<h3 className="font-bold text-[22px] mb-4 break-all sm:break-normal tracking-[-0.5px]">{title}</h3>
+				<h3 className="font-bold text-[22px] mb-4 break-all sm:break-normal leading-[28px] tracking-[-0.5px]">
+					{title}
+				</h3>
 				<p className="block mb-6 sm:mb-0 break-all sm:break-normal tracking-[-0.5px]">{description}</p>
 				{tags && tags.length > 0 ? <TagList tags={tags} customStyle="sm:absolute sm:bottom-0 sm:left-0" /> : null}
 				<span className="text-[15px] font-medium absolute bottom-0 right-0 dark:text-darkText">{date}</span>
