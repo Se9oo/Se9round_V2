2277f99146ea30cc00df95bef9c307acfda7cb6c
se9oo <se9oo@kakao.com>
2023-02-14 12:05:45 +0900

    eslint > import/prefer-default-export 옵션 off

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export const HEADER_HEIGHT = 72;
 export const PAGINATION_LIMIT_COUNT = 5;
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,3 @@
-/* eslint-disable import/prefer-default-export */
-
 /**
  *
  * @param {string} text
