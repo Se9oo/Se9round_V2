eca2f6e58163f46bad8dc7fcee1adaa1da4c05f3
se9oo <se9oo@kakao.com>
2023-02-15 15:30:28 +0900

    날짜 포맷 변경 함수 추가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -21,3 +21,37 @@ export const generateSlug = (str: string) => {
 
 	return text;
 };
+
+/**
+ *
+ * @param {string} date
+ * @param {kor | hyphen} type
+ * @returns date formatter
+ */
+export const getFormattedDate = (date: string, type: 'kor' | 'hyphen') => {
+	if (!date) {
+		return date;
+	}
+
+	const FORMATTED_DATE_LENGTH = 8;
+
+	const newDate = date.replace(/[-\D]/g, '');
+
+	if (newDate.length !== FORMATTED_DATE_LENGTH) {
+		return newDate;
+	}
+
+	const year = newDate.substring(0, 4);
+	const month = newDate.substring(4, 6);
+	const day = newDate.substring(6, 8);
+
+	if (type === 'kor') {
+		return `${year}년 ${month}월 ${day}일`;
+	}
+
+	if (type === 'hyphen') {
+		return `${year}-${month}-${day}`;
+	}
+
+	return newDate;
+};
