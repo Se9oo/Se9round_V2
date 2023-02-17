4ae8fe8ea26e753dd8f4951aee512f1411027eb7
se9oo <se9oo@kakao.com>
2023-02-17 16:00:15 +0900

    Tag 컴포넌트 > next/link prefetch false

--- src/components/tags/TagItem.tsx
+++ src/components/tags/TagItem.tsx
@@ -8,6 +8,7 @@ const TagItem = ({ tag }: { tag: string }) => {
 			className="block rounded-[4px] bg-main px-2 py-[0.5px]"
 			href={{ pathname: PAGE_URLS.TAGS, query: { tagName: tag } }}
 			as={PAGE_URLS.TAGS}
+			prefetch={false}
 		>
 			<span className="text-xs sm:text-sm font-bold text-white dark:text-darkText">{tag}</span>
 		</Link>
