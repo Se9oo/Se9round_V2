09c0441e54a835d9b1787a7207dca0c0dc0d827d
se9oo <se9oo@kakao.com>
2023-03-29 11:37:00 +0900

    google search console validation key 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 				/>
 				<title>se9round.dev</title>
 				<meta name="description" content="se9oo 개발 블로그" />
+				<meta name="google-site-verification" content="Tnf1TW2CuqiBCgfP-BC9qkilgf8ab3QztcxLjBrfFSI" />
 				<script
 					src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
 					integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
