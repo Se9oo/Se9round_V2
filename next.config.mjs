406e46d4c7ad233e4f83b3c114c60c884ca7ce7e
se9oo <se9oo@kakao.com>
2026-08-05 20:41:48 +0900

    chore: MDX 파이프라인 설정 (@next/mdx + rehype-pretty-code + shiki)

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+import createMDX from '@next/mdx';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+	images: {
+		deviceSizes: [768, 1370],
+		remotePatterns: [{ hostname: 'hfjaydlcifnsisqntesa.supabase.co' }],
+	},
+};
+
+const withMDX = createMDX({
+	options: {
+		rehypePlugins: [
+			['rehype-pretty-code', { theme: 'material-theme-palenight' }],
+			['rehype-slug'],
+		],
+	},
+});
+
+export default withMDX(nextConfig);
