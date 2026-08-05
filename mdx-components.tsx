498d67a3ef610ba2572f937b946288e4efd6307f
se9oo <se9oo@kakao.com>
2026-08-05 20:57:49 +0900

    feat: mdx-components.tsx 포팅 및 CodePre 컴포넌트 작성

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,48 @@
 import type { MDXComponents } from 'mdx/types';
+import CodePre from '@/components/posts/CodePre';
+import MarkdownImage from '@/components/posts/MarkdownImage';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-	return { ...components };
+	return {
+		pre: CodePre,
+		blockquote: ({ children }) => (
+			<blockquote className="my-4 border-l-4 border-l-[#8491D9] bg-[#F8F9FA] p-4 dark:bg-[#282C34]">
+				{children}
+			</blockquote>
+		),
+		a: ({ href, children }) => {
+			if (href?.match('http')) {
+				return (
+					<a
+						href={href}
+						target="_blank"
+						rel="noopener noreferrer"
+						style={{ color: '#8491D9', fontWeight: 700, wordBreak: 'break-word', textDecoration: 'underline' }}
+					>
+						{children}
+					</a>
+				);
+			}
+			return <a href={href}>{children}</a>;
+		},
+		ol: ({ children }) => <ol style={{ listStyle: 'decimal', marginLeft: '1rem' }}>{children}</ol>,
+		ul: ({ children }) => (
+			<ul style={{ listStyle: 'disc', marginLeft: '2rem', paddingBottom: '0.5rem' }}>{children}</ul>
+		),
+		hr: (props) => <hr className="text-grey dark:text-greyLight" {...props} />,
+		strong: ({ children }) => (
+			<strong
+				style={{
+					fontFamily: 'Monaco, Spoqa Han Sans Neo, Noto Sans KR, sans-serif',
+					fontWeight: 600,
+					verticalAlign: '0.5px',
+					fontSize: '1rem',
+				}}
+			>
+				{children}
+			</strong>
+		),
+		img: MarkdownImage,
+		...components,
+	};
 }
---
+++ src/components/posts/CodePre.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useState, useRef } from 'react';
+import CopyIcon from '../icons/CopyIcon';
+import CheckIcon from '../icons/CheckIcon';
+
+const CodePre = ({ children, ...props }: React.ComponentPropsWithoutRef<'pre'>) => {
+	const [showCopy, setShowCopy] = useState(false);
+	const [isCopy, setIsCopy] = useState(false);
+	const preRef = useRef<HTMLPreElement>(null);
+
+	const handleCopy = () => {
+		navigator.clipboard.writeText(preRef.current?.textContent ?? '');
+		setIsCopy(true);
+	};
+
+	const handleMouseLeave = () => {
+		setShowCopy(false);
+		if (isCopy) setTimeout(() => setIsCopy(false), 500);
+	};
+
+	return (
+		<div
+			className="relative my-4 mb-8"
+			onMouseEnter={() => setShowCopy(true)}
+			onMouseLeave={handleMouseLeave}
+			onFocus={() => setShowCopy(true)}
+			onBlur={handleMouseLeave}
+		>
+			<pre ref={preRef} {...props}>
+				{children}
+			</pre>
+			<button
+				type="button"
+				onClick={handleCopy}
+				className={`${showCopy ? 'block' : 'hidden'} absolute top-3 right-3 rounded border border-greyCopy p-1
+					hover:bg-greyCopy`}
+			>
+				{isCopy ? <CheckIcon className="text-darkText" /> : <CopyIcon className="text-darkText" />}
+			</button>
+		</div>
+	);
+};
+
+export default CodePre;
