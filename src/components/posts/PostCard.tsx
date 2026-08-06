8e8626afb62efce7dfd5e22e8068ddbd61c52f60
sehyun <se9oo@kakao.com>
2026-08-06 16:31:30 +0900

    fix: PostCard 왼쪽 영역 폭 고정 안 되던 오타 수정

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -10,13 +10,33 @@ const PostCard = ({ metaData }: { metaData: PostMetaDataType }) => {
 	const { title, description, date, tags, socialImage } = metaData;
 
 	return (
-		<article>
+		<article className="group">
 			<Link
 				href={`${PAGE_URLS.POST}/${convertSpaceToDash(title)}`}
 				className="z-content flex w-full cursor-pointer flex-col sm:h-[172px] sm:flex-row sm:justify-between"
 			>
+				<div className="relative order-2 flex h-full flex-col sm:order-1 sm:w-[65%]">
+					<h3
+						className="mb-4 text-[22px] leading-[28px] font-bold tracking-[-0.5px] break-all group-hover:text-main
+							sm:break-normal"
+					>
+						{title}
+					</h3>
+					<p
+						className="mb-6 block tracking-[-0.5px] break-keep text-greyCopy sm:mb-0 sm:break-normal
+							dark:text-darkText/80"
+					>
+						{description}
+					</p>
+					{tags && tags.length > 0 ? <TagList tags={tags} customStyle="sm:absolute sm:bottom-0 sm:left-0" /> : null}
+					<span className="absolute right-0 bottom-0 text-[15px] leading-3 font-medium text-greyCopy dark:text-darkText">
+						{getFormattedDate(date, 'hyphen')}
+					</span>
+				</div>
 				{socialImage && (
-					<div className="relative mr-2 mb-6 aspect-video w-full sm:mr-12 sm:mb-0 sm:h-auto sm:w-[36%]">
+					<div
+						className="relative order-1 mb-6 ml-2 aspect-video w-full sm:order-2 sm:mb-0 sm:ml-12 sm:h-auto sm:w-[36%]"
+					>
 						<Image
 							src={socialImage}
 							alt="post-thumbnail"
@@ -29,16 +49,6 @@ const PostCard = ({ metaData }: { metaData: PostMetaDataType }) => {
 						/>
 					</div>
 				)}
-				<div className="relative flex h-full flex-col sm:w-[65%]">
-					<h3 className="mb-4 text-[22px] leading-[28px] font-bold tracking-[-0.5px] break-all sm:break-normal">
-						{title}
-					</h3>
-					<p className="mb-6 block tracking-[-0.5px] break-all sm:mb-0 sm:break-normal">{description}</p>
-					{tags && tags.length > 0 ? <TagList tags={tags} customStyle="sm:absolute sm:bottom-0 sm:left-0" /> : null}
-					<span className="absolute right-0 bottom-0 text-[15px] leading-3 font-medium dark:text-darkText">
-						{getFormattedDate(date, 'hyphen')}
-					</span>
-				</div>
 			</Link>
 		</article>
 	);
